f6efe4e614e91f3301ee2744dafc5336da1446e0
kosmosCS <gani.akhmerov@gmail.com>
2016-02-05 21:07:19 +0500

    semaphore update2

--- semsysV.c
+++ semsysV.c
@@ -16,19 +16,35 @@
 #include <sys/shm.h>
 
 #define M_PATH "sem.temp"
+union semun {
+    int val;
+    unsigned short *array;
+};
 
 int main(int argv, char **argc) {
     key_t key;
     int semID;
     int i;
     struct sembuf sops[16];
+    union semun a,b;
+    unsigned short v[100];
     do {
         key = ftok(M_PATH,1);
     }while(key == -1);
-   
-    semID = semget(key, 16, 0660 | IPC_CREAT);
-    printf("key=%d\tsemID=%d\n", key, semID);
 
+    semID = semget(key, 16, 0660 | IPC_CREAT);
+    perror("Errno after semop: ");
+    a.array = 0;
+    b.val = 0;
+/*    for(i = 0; i<100; i++) {
+        printf("exist sem# %d\n", v[i]);
+        semctl(semID, i, IPC_RMID, b);
+        perror("Errno after delete: ");
+    }*/
+
+        semctl(semID, 0, SETALL, b);
+        perror("Errno after delete: ");
+    printf("key=%d\tsemID=%d\n", key, semID);
     for(i = 0; i < 16; i++) {
         sops[i].sem_num = i;
         sops[i].sem_op = i;
@@ -36,6 +52,6 @@ int main(int argv, char **argc) {
     }
     semop(semID, sops, 16);
     perror("Errno after semop: ");
-    while(1);
+//    while(1);
 
 }
